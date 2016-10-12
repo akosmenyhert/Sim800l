04ac2e939d647cc9dbfff6f0bf27ed65ca229e2d
L Nix <lornix@lornix.com>
2016-10-11 23:19:38 -0700

    Pull comments to separate lines, cleanup

--- Sim800l.h
+++ Sim800l.h
@@ -1,16 +1,16 @@
 /* this library is writing by  Cristian Steib.
  *      steibkhriz@gmail.com
- *  Designed to work with the GSM Sim800l,maybe work with SIM900L
+ *  Designed to work with the GSM Sim800l, maybe work with SIM900L
  *
- *     This library use SoftwareSerial, you can define de RX and TX pin
- *     in the header "Sim800l.h" ,by default the pin is RX=10 TX=11..
- *     be sure that gnd is attached to arduino too.
- *     You can also change the other preferred RESET_PIN
+ *   This library use SoftwareSerial, you can define RX and TX pin in the
+ *   header "Sim800l.h" ,by default the pin is RX=10 TX=11.. be sure that gnd
+ *   is attached to arduino too.
+ *   You can also change the other preferred RESET_PIN
  *
- *     Esta libreria usa SoftwareSerial, se pueden cambiar los pines de RX y TX
- *     en el archivo header, "Sim800l.h", por defecto los pines vienen configurado en
- *     RX=10 TX=11.
- *     Tambien se puede cambiar el RESET_PIN por otro que prefiera
+ *   Esta libreria usa SoftwareSerial, se pueden cambiar los pines de RX y TX
+ *   en el archivo header, "Sim800l.h", por defecto los pines vienen
+ *   configurado en RX=10 TX=11.
+ *   Tambien se puede cambiar el RESET_PIN por otro que prefiera
  *
  *    PINOUT:
  *        _____________________________
@@ -30,49 +30,57 @@
  */
 
 #ifndef Sim800l_h
+
 #define Sim800l_h
 #include <SoftwareSerial.h>
 #include "Arduino.h"
 
-#define RX_PIN 10
-#define TX_PIN 11
-#define RESET_PIN 2   // pin to the reset pin sim800l
+#define RX_PIN    10
+#define TX_PIN    11
+// pin to the reset pin sim800l
+#define RESET_PIN 2
 
-#define LED true // used for indicator led, in case that you don want set to false .
-#define LED_PIN 13 //pin to indicate states.
+// used for indicator led, in case that you don want set to false .
+#define LED     true
+//pin to indicate states.
+#define LED_PIN 13
 
-class Sim800l
-{
- private:
-     int _timeout;
-     String _buffer;
-     String _readSerial();
+class Sim800l {
+  private:
+    int    _timeout;
+    String _buffer;
+    String _readSerial();
 
- public:
-     void begin();
-     void reset();
+  public:
+    void begin();
+    void reset();
 
-     // Methods for calling || Funciones de llamadas.
-     bool answerCall();
-     void callNumber(char* number);
-     bool hangoffCall();
-     uint8_t getCallStatus();
+    // Methods for calling || Funciones de llamadas.
+    bool    answerCall();
+    void    callNumber(char *number);
+    bool    hangoffCall();
+    uint8_t getCallStatus();
 
-     //Methods for sms || Funciones de SMS.
-     bool sendSms(char* number,char* text);
-     String readSms(uint8_t index); //return all the content of sms
-     String getNumberSms(uint8_t index); //return the number of the sms..
-     bool delAllSms();     // return :  OK or ERROR ..
+    // Methods for sms || Funciones de SMS.
+    bool sendSms(char *number, char *text);
+    // return all the content of sms
+    String readSms(uint8_t index);
+    // return the number of the sms..
+    String getNumberSms(uint8_t index);
+    // return :  OK or ERROR ..
+    bool delAllSms();
 
-     void signalQuality();
-     void setPhoneFunctionality();
-     void activateBearerProfile();
-     void deactivateBearerProfile();
+    void signalQuality();
+    void setPhoneFunctionality();
+    void activateBearerProfile();
+    void deactivateBearerProfile();
 
-     //get time with the variables by reference
-     void RTCtime(int *day,int *month, int *year,int *hour,int *minute, int *second);
-     String dateNet(); //return date,time, of the network
-     bool updateRtc(int utc);  //Update the RTC Clock with de Time AND Date of red-.
+    // get time with the variables by reference
+    void RTCtime(int *day, int *month, int *year, int *hour, int *minute, int *second);
+    // return date,time, of the network
+    String dateNet();
+    // Update the RTC Clock with de Time AND Date of red-.
+    bool updateRtc(int utc);
 };
 
 #endif
